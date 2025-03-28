17c435587f4555174f6d723515354d5beb56c660
laboratorio <laboratorio@UPOAULA10713>
2025-03-28 07:24:10 -0500

    Agregar archivos de proyecto.

---
+++ FeijooJ-MiPrimerProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeijooJ_MiPrimerProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
